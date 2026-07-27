80e20c981b41201f3d53597f145a514489b6673d
Nitish Chauhan <125082438+nitishchauhan90@users.noreply.github.com>
2026-07-28 00:33:07 +0530

    Time: 0 ms (100%), Space: 31.4 MB (46.95%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -7,8 +7,7 @@ public:
         while(low<high){
             int mid = low + (high-low)/2;
             if(nums[mid] == target){
-                result = mid;
-                low = mid+1;
+                return mid;
             }
             else if (nums[mid] < target) {
                 low = mid + 1;
